90c07986fbb996bfac65adaa79e50cf4797d7b18
Cameron Harding <cahard02@wsc.edu>
2021-12-16 21:28:27 -0600

    updated docstring

--- main.py
+++ main.py
@@ -1,42 +1,18 @@
 #!/usr/bin/env python3
 """
 NAME
-    main_menu - Chapter 6 Assignment
+    Game21 - Scripting Languages Final
 
 DESCRIPTION
-    Student Data System for managing student information (student id, first name, and last name)
-
-    This module contains the functions for displaying the main menu and running the menu options
-
-FUNCTIONS
-    display_menu()
-        Displays a list of all the valid main menu options
-        It also handles for nonnumerical data and invalid menu option selected.
-
-        1 - List all students
-        2 - Add a student
-        3 - Update a student
-        4 - Delete a student
-        0 - Exit program
-
-        :return: no value
-        :rtype: none
-
-    main()
-        Main keeps the program looping until the user enters 0 to exit the program
-        then based on the user's selected, will call the corresponding function option
-        Local scoped students is a 2D list that is pass as an argument to each menu option function
-        Local scoped max_student_id is the last student id used, and is passed to the add_student function,
-        and this function will return the last added student id
-
-        :return: no value
-        :rtype: none
+    A fun game similar to blackjack: each player bets 25 or 50 cents,
+    drawing cards to get as close to 21 as possible without going over.
+    The players play against a computer dealer.
 
 VERSION
     1.0
 
 DATE
-    2021.10.31
+    2021.12.10
 
 AUTHOR
     Penelope Harding
